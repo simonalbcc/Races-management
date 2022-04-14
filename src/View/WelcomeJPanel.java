45f05c6a15ce1348db90b57e40687ef8b0928cfc
AlexisDeWinter <100135893+AlexisDeWinter@users.noreply.github.com>
2022-04-14 15:53:20 +0200

    premiere bordure + movement centralisé

--- src/View/WelcomeJPanel.java
+++ src/View/WelcomeJPanel.java
@@ -1,9 +1,13 @@
 package View;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class WelcomeJPanel extends JPanel {
@@ -71,16 +75,91 @@ public class WelcomeJPanel extends JPanel {
 
     private class CarPanel extends JPanel{
         public GraphicCar graphicCar;
+        public Wall leftWall, rightWall;
 
         public CarPanel(){
-            setLayout(new FlowLayout());
+            setLayout(new BorderLayout());
             graphicCar = new GraphicCar();
+            leftWall = new Wall(0,0,10,150);
+            rightWall = new Wall(975,0,10,150);
             MovementCarThread movementCarThread = new MovementCarThread(this, graphicCar);
             movementCarThread.start();
         }
         public void paint(Graphics g){
             super.paint(g);
+            leftWall.draw(g);
+            rightWall.draw(g);
             graphicCar.paint(g);
+        }
+
+        private class MovementCarThread extends Thread{
+            private JPanel panel;
+            private GraphicCar graphicCar;
+
+            public MovementCarThread(JPanel panel, GraphicCar graphicCar){
+                this.panel = panel;
+                this.graphicCar = graphicCar;
+            }
+
+            public void run(){
+                while(true){
+                    try{
+                        Thread.sleep(15);
+                        this.graphicCar.move();
+                        this.panel.repaint();
+                    }
+                    catch (InterruptedException exception){
+                        exception.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        private class GraphicCar extends JPanel {
+            private BufferedImage carImage;
+            private Rectangle rectangle;
+            private int deltaX = 1;
+
+            public GraphicCar(){
+                try{
+                    carImage = ImageIO.read(new FileInputStream("car.png"));
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                repaint();
+                rectangle = new Rectangle(11,0,120,100);
+            }
+
+            public void move(){
+                if(carPanel.rightWall.collision(this) || carPanel.leftWall.collision(this)){
+                    deltaX *= -1;
+                }
+                rectangle.x += deltaX;
+            }
+
+            public void paint(Graphics g){
+                g.drawImage(carImage, rectangle.x, rectangle.y, rectangle.width, rectangle.height, null );
+            }
+
+            public Rectangle getRectangle() {
+                return rectangle;
+            }
+        }
+
+        private class Wall extends JPanel{
+            private Rectangle rectangle;
+
+            public Wall(int x, int y, int width, int height){
+                this.rectangle = new Rectangle(x,y,width,height);
+            }
+
+            public void draw(Graphics g){
+                g.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+            }
+
+            public boolean collision(GraphicCar graphicCar){
+                return this.rectangle.intersects(graphicCar.getRectangle());
+            }
         }
     }
 }
